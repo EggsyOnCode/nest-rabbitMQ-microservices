e8214adfc099ce5290d53a4dc507d4f28f17d073
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-23 17:54:22 +0500

    added rabbitMQ service and created billings module

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { RabbitMQModule } from '@app/commons';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    RabbitMQModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
--- libs/commons/src/index.ts
+++ libs/commons/src/index.ts
@@ -1,3 +1,5 @@
 export * from './database/database.module';
 export * from './database/abstract.schema';
 export * from './database/abstract.repository';
+export * from './rabbitMQ/rabbitMQ.service';
+export * from './rabbitMQ/mq.module';
