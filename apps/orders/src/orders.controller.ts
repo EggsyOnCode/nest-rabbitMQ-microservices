497845fbf8f81a2db38cff8d11ff460ee340013a
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-23 19:46:04 +0500

    instantiated CLintProxy in the orders services class for comms with Billing microservice

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrderRequestDTO } from './dtos/create-order.request';
+import { BILLING_SERIVCE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    private readonly ordersService: OrdersService,
+    @Inject(BILLING_SERIVCE) readonly billingClient: ClientProxy,
+  ) {}
 
   @Post()
   async createOrder(@Body() request: OrderRequestDTO) {
@@ -14,5 +19,10 @@ export class OrdersController {
   @Get()
   async getOrders() {
     return this.ordersService.getOrders();
+  }
+
+  @Post()
+  async createBill() {
+    return null;
   }
 }
--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,10 +3,12 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/commons';
+import { DatabaseModule, RmqModule } from '@app/commons';
 import { OrdersRepository } from './order.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { BillingService } from 'apps/billing/src/billing.service';
+import { BILLING_SERIVCE } from './constants/services';
 
 @Module({
   imports: [
@@ -15,12 +17,17 @@ import { Order, OrderSchema } from './schemas/order.schema';
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
         PORT: Joi.number().required(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
       }),
       envFilePath: './apps/orders/.env',
     }),
     //since we need to perform CRUD operations on MongoDB
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    //using the rmq module to register the billing service so taht we can pass on msgs from orders to the billign service
+    RmqModule.register({ name: BILLING_SERIVCE }),
+    BillingService,
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
